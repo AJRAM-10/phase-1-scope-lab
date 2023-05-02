712bd0a5ccdadd7a109cefe3264f8369b2d350e7
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-05-02 13:54:27 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'John Doe'
+
+function upperCaseCustomerName(){
+    return customerName = 'BOB'
+}
+
+function setBestCustomer (){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'Bob'
+}
